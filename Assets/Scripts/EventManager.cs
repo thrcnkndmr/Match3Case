321c04b9680dd6e5af7c2c615d9b464b79fae441
Tahircan Kandemir <thrcnkndmr@outlook.com>
2023-11-03 00:32:36 +0300

    +

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -3,9 +3,15 @@ using System;
 public static class EventManager
 {
    public static event Action OnStartGameEvent;
+   public static event Action OnMoveItemEvent;
 
    public static void OnStartGameInvoker()
    {
       OnStartGameEvent?.Invoke();
+   }
+
+   private static void OnMoveItemEventInvoker()
+   {
+      OnMoveItemEvent?.Invoke();
    }
 }
